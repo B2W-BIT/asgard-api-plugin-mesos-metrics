108b3fce8398817766e64296e419b4a828dcb413
Dalton Barreto <daltonmatos@gmail.com>
2018-04-11 11:51:52 -0300

    Logando o IP do leader encontrado
    IN-2454

--- metrics/util.py
+++ metrics/util.py
@@ -11,7 +11,7 @@ def get_mesos_leader_address():
             response = requests.get(f"{mesos_address}/redirect", timeout=2, allow_redirects=False)
             if response.headers.get("Location"):
                 leader_ip = response.headers.get("Location").split("//")[1]
-                config.logger.debug({"action": "find-mesos-leader", "try-address": mesos_address, "exception": False, "leader": True})
+                config.logger.debug({"action": "find-mesos-leader", "try-address": mesos_address, "exception": False, "leader-ip": leader_ip})
                 return f"http://{leader_ip}"
         except requests.exceptions.ConnectionError as ConErr:
             config.logger.debug({"action": "find-mesos-leader", "try-address": mesos_address, "exception": True})
